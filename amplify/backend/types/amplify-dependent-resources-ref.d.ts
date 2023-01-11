b715f269cd646ac1a890a1d4fbf22fbdef756d76
Thomas <thomas-for-aiven@februus.net>
2023-01-11 16:44:47 +0100

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "kgreenamp50ef649aPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "kgreenamp2ca15f9e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {
+  View,
+  Authenticator,
+  withAuthenticator,
+} from '@aws-amplify/ui-react';
+import Button from '@mui/material/Button';
+// import { Button, IconButton } from '@mui/icons-material';
+
+
+Amplify.configure(awsconfig);
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>STUFF</h2>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <p class="Username">Hello {user.username}</p>
+          <button onClick={signOut}>Sign out</button>
+          <p class="center">Welcome to kelvin.green</p>
+        </main>
+      )}
+    </Authenticator>
+    <Button variant="contained">Upload</Button>
+    </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App); 
